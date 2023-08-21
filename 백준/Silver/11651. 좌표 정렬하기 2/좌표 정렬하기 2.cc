eab52c082146254039e6c4e07ae3fdd1893803ee
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2023-08-21 20:44:19 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,27 @@
+//좌표 정렬하기
+#include <iostream>
+#include <string> // getline사용
+#include <algorithm> // sort사용
+#include <cmath> // 거듭제곱수구하기
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N;
+    pair<int, int> point[100000];
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> point[i].second >> point[i].first;
+    }
+    
+    sort(point, point+N);
+
+    for(int i=0; i<N; i++){
+        cout << point[i].second << " "<< point[i].first << "\n";
+    }
+}
