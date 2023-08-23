295d32af63b579bc90fc3c470c130287826b382f
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2023-08-23 16:12:33 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 308 ms, Memory: 8296 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,35 @@
+//숫자 카드2
+#include <iostream>
+#include <vector>
+#include <algorithm> // sort사용
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    vector<int> num1;
+    vector<int> num2;
+
+    int N, temp, count = 0;
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> temp;
+        num1.push_back(temp);
+    }
+
+    int M;
+    cin >> M;
+    for(int i=0; i<M; i++){
+        cin >> temp;
+        num2.push_back(temp);
+    }
+    sort(num1.begin(), num1.end());
+
+    for(int i=0; i<M; i++){
+        cout << upper_bound(num1.begin(),num1.end(),num2[i])-lower_bound(num1.begin(),num1.end(),num2[i]) << " ";
+    } 
+}
