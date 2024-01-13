59d8fd4c2cadf8ea0d3172d51c54093ba0547643
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2024-01-13 20:30:28 +0900

    [Silver I] Title: 회의실 배정, Time: 88 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <tuple>
-#include <vector>
 #include <algorithm>
 using namespace std;
-
 
 int main()
 {
     int N, start, end;
     pair<int, int> time[100000];
-    vector<pair<int, int>> fix;
-    bool check = true;
+    int count = 1;
+    int last_end;
 
     cin >> N;
     for (int i = 0; i < N; i++) {
@@ -21,21 +18,13 @@ int main()
 
     sort(time, time + N);
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < fix.size(); j++) {
-            if (time[i].second < fix[j].first) {
-                check = false;
-                break;
-            }
-        }
-        if (check == false) {
-            check = true;
-            continue;
-        }
-        else {
-            fix.push_back(make_pair(time[i].first, time[i].second));
+    last_end = time[0].first;
+    for (int i = 1; i < N; i++) {
+        if (time[i].second >= last_end) {
+            count++;
+            last_end = time[i].first;
         }
     }
 
-    cout << fix.size();
+    cout << count;
 }
