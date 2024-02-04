5bd9a731706a288a5b3bf7b84f25e0075d7f33e9
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2024-02-04 15:11:37 +0900

    [Silver II] Title: 제곱수의 합, Time: 40 ms, Memory: 2304 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n, temp, min = 2100000000;
+	int dp[100001];
+
+	cin >> n;
+
+	dp[0] = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = floor(sqrt(i)); j > 0; j--) {
+			temp = 1 + dp[i - int(pow(j, 2))];
+			if (min > temp) {
+				min = temp;
+			}
+		}
+		dp[i] = min;
+		min = 2100000000;
+	}
+
+	cout << dp[n];
+}
