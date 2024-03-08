c5b7f48b193745bded9e271cbf1b56b5d1060000
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2024-03-08 12:56:04 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue> 
+#include <string>
+using namespace std;
+
+queue<pair<int, int>> bfs;
+bool arr[101][101];
+int depth[101][101];
+
+int main()
+{
+	string temp;
+	int N, M;
+	int x, y;
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		cin >> temp;
+		for (int j = 0; j < M; j++) {
+			if (temp[j] == '1') {
+				arr[i][j+1] = true;
+			}
+			else {
+				arr[i][j+1] = false;
+			}
+		}
+	}
+	
+	bfs.push(make_pair(1, 1));
+	depth[1][1] = 1;
+	arr[1][1] = false;
+	while (true)
+	{
+		x = bfs.front().first;
+		y = bfs.front().second;
+		if (x == N && y == M) {
+			break;
+		}
+		bfs.pop();
+		if (arr[x + 1][y] == true && x + 1 <= N) {
+			bfs.push(make_pair(x + 1, y));
+			depth[x + 1][y] = depth[x][y] + 1;
+			arr[x + 1][y] = false;
+		}
+		if (arr[x - 1][y] == true && x - 1 >= 1) {
+			bfs.push(make_pair(x - 1, y));
+			depth[x - 1][y] = depth[x][y] + 1;
+			arr[x - 1][y] = false;
+		}
+		if (arr[x][y + 1] == true && y + 1 <= M) {
+			bfs.push(make_pair(x, y + 1));
+			depth[x][y + 1] = depth[x][y] + 1;
+			arr[x][y + 1] = false;
+		}
+		if (arr[x][y - 1] == true && y - 1 >= 1) {
+			bfs.push(make_pair(x, y - 1));
+			depth[x][y - 1] = depth[x][y] + 1;
+			arr[x][y - 1] = false;
+		}
+	}
+	cout << depth[N][M];
+}
