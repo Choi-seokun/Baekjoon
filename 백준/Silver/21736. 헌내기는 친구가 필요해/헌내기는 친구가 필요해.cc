c56cd83ea1a1764bf4e8117e300bccf95e125805
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2024-03-09 19:13:24 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 12 ms, Memory: 2860 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -14,6 +14,10 @@ int main()
 	int yy[4] = { 0,1,0,-1 };
 	queue<pair<int, int>> bfs;
 
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
