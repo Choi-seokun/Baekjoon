eb835a235cec6ee66a8d32e2491cbeb83799ff0f
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2025-10-06 17:16:42 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string N;
+    cin >> N;
+
+    sort(N.begin(), N.end(), greater<int>());
+
+    cout << N;
+    return 0;
+}
