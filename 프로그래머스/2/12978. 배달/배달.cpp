f7f7b3f3949215e62ad90807f07eb2a20c1c2ac0
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2026-04-22 13:29:39 +0900

    [level 2] Title: 배달, Time: 0.32 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int solution(int N, vector<vector<int> > road, int K) {
+    vector<pair<int, int>> adj[51];
+    
+    for(auto& r : road){
+        adj[r[0]].push_back({r[1], r[2]});
+        adj[r[1]].push_back({r[0], r[2]});
+    }
+    
+    vector<int> dist(N+1, INT_MAX);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    
+    dist[1] = 0;
+    pq.push({0, 1});
+    
+    while(!pq.empty()){
+        int d = pq.top().first;
+        int curr = pq.top().second;
+        pq.pop();
+        
+        if(d > dist[curr]){
+            continue;
+        }
+        
+        for(auto& edge : adj[curr]){
+            int next = edge.first;
+            int cost = edge.second;
+            if(dist[next] > d + cost){
+                dist[next] = d + cost;
+                pq.push({dist[next], next});
+            }
+        }
+    }
+    
+    int answer = 0;
+    for(int i = 1; i < N+1; i++){
+        if(dist[i] <= K){
+            answer++;
+        }
+    }
+    
+    return answer;
+}
